7f699f390e6320b5a7f0289d5ddcb3e5c9c88a98
sonamnishad123 <sonamnishad789@gmail.com>
2026-02-25 19:10:59 +0530

    comparison operator

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-let a=10; let b=20; var c=10; var linebreak="<br>";
-document.write("a+b+c=");
-let result=a+b+c;
-document.write(result); 
-document.write(linebreak);
 
-document.write("a-b=");
-result=a-b;
+// document.write("a+b+c=");
+// let result=a+b+c;
+// document.write(result); 
+// document.write(linebreak);
+
+// document.write("a-b=");
+// result=a-b;
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("a/b=");
+// result=a/b;
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("a%b=");
+// result=a%b;
+// document.write(result);
+// document.write(linebreak);
+let a=10; let b=20; var linebreak="<br>";
+document.write("(a==b)=>");
+result=(a==b);
 document.write(result);
 document.write(linebreak);
 
-document.write("a/b=");
-result=a/b;
+document.write("(a>b)=>");
+result=(a>b);
 document.write(result);
 document.write(linebreak);
 
-document.write("a%b=");
-result=a%b;
+document.write("(a>=b)=>");
+result=(a>=b);
 document.write(result);
 document.write(linebreak);
+
+document.write("(a<=b)=>");
+result=(a<=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a!=b)=>");
+result=(a!=b);
+document.write(result);
+document.write(linebreak);
+
