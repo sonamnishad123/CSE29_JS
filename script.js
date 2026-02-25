59c0ba862e3a85a593db3bb1df1bc4d4f31fbb11
sonamnishad123 <sonamnishad789@gmail.com>
2026-02-25 19:44:50 +0530

    logical operator

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 let x=10; y=20; let linebreak="<br>";
 
-document.write("Value of x=>(x=y)=>");
-let result=(x=y);
+// document.write("Value of x=>(x=y)=>");
+// let result=(x=y);
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("Value of x=>(x+=y)=>");
+// result=(x+=y);
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("Value of x=>(x-=y)=>");
+// result=(x-=y);
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("Value of x=>(x*=y)=>");
+//  result=(x*=y);
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("Value of x=>(x/=y)=>");
+//  result=(x/=y);
+// document.write(result);
+// document.write(linebreak);
+document.write("(x=15 && y<15)=>")
+result=(x=15 && y<15);
 document.write(result);
 document.write(linebreak);
 
-document.write("Value of x=>(x+=y)=>");
-result=(x+=y);
+document.write("(x==20 || y<15)=>")
+result=(x==20 || y<15);
 document.write(result);
 document.write(linebreak);
 
-document.write("Value of x=>(x-=y)=>");
-result=(x-=y);
+document.write("!(x==y)=>")
+result=(!(x==y));
 document.write(result);
 document.write(linebreak);
-
-document.write("Value of x=>(x*=y)=>");
- result=(x*=y);
-document.write(result);
-document.write(linebreak);
-
-document.write("Value of x=>(x/=y)=>");
- result=(x/=y);
-document.write(result);
-document.write(linebreak);
-
 
