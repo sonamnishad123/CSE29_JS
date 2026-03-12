babab2202553975be53d693cc143dd0caa139377
sonamnishad123 <sonamnishad789@gmail.com>
2026-03-12 13:04:17 +0530

    classwork11/3/26

--- script.js
+++ script.js
@@ -39,9 +39,67 @@
 // document.write(result);
 // document.write(linebreak);
 
-let x=10; y=20; c=18; let linebreak="<br>";
-document.write("greatest of three numbers is:")
-let result=(x>y)?((x>c)?("x is greatest"):("c is greatest")):((y>c)?("y is greatest"):("c is greatest"));
-document.write(result);
-document.write(linebreak);
+// let x=10; y=20; c=18; let linebreak="<br>";
+// document.write("greatest of three numbers is:")
+// let result=(x>y)?((x>c)?("x is greatest"):("c is greatest")):((y>c)?("y is greatest"):("c is greatest"));
+// document.write(result);
+// document.write(linebreak);
+ 
+// let count;
+// document.write("Starting Loop"+"<br/>");
+// for(count=0;count<10;count++){
+//     document.write("Current Count: "+count);
+//     document.write("<br/>");
+// }
+// document.write("loop stopped");
 
+
+// const person={
+//     fname:"Ajay",
+//     Iname:"Singh",
+//     age:25
+// };
+// for(let x in person){
+//     console.log("person details:"+x+":"+person[x])
+// }
+
+// let age=20;
+// if(age>18){
+//     document.write("<b>You are eligible to cast the vote.</b>");
+// }
+// var grade='A';
+// document.write("Entering switch block<br/>");
+// switch(grade){
+//     case 'A':document.write("Good job<br/>");
+//     break;
+//     case 'B':document.write("Pretty good<br/>");
+//     break;
+//     case 'C':document.write("Passed<br/>");
+//     break;
+//     case 'D':document.write("Not so good<br/>");
+//     break;
+//     case 'F':document.write("Failed<br/>");
+//     break;
+//     default:document.write("Unknown grade<br/>");
+// }
+// document.write("Exiting switch block");
+// function myFunction(){
+//     alert("HEllO WORLD")
+// }
+// let add=(a,b)=>{
+//     console.log(a+b);
+// }
+// add(10,20);
+// var person={
+//     name:"John Mac",
+//     age:30,
+//     isMarried:true,
+//     address:{
+//         street:'Vinay nagar',
+//         flatNo:201,
+//     }
+// };
+// console.log(person);
+var myinfo=new Array();
+var myinfo1=Array();
+console.log(myinfo,myinfo1);
