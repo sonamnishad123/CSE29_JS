299096bda42118d317a0aef495f17c14ab29fda2
sonamnishad123 <sonamnishad789@gmail.com>
2026-04-08 15:01:33 +0530

    MT-1

---
+++ scriptA.js
@@ -0,0 +1,18 @@
+// const x=document.getElementById("p1");
+// x.innerHTML="Sonam Nishad";
+
+// const result=document.getElementById("para");
+// result.innerHTML="my self";
+// result.title="myname";
+
+// const result=document.getElementsByClassName("p1");
+// console.log(result);
+
+// result[1].innerhtml="nishad";
+// result[0].innerhtml="sonam";
+
+// const x=document.getElementById("sn").getAttribute("class");
+
+// document.getElementById("d").innerHTML=x;
+
+// var text='{"employees":{'+'{"firstnaame":"John","lastname":"Dor"},'+'{"firstname":"Anna","lastname":"Smith"},'
